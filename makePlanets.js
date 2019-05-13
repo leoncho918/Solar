b1e72b59d7ab80eca1139fcbc79abc4062e774db
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2019-05-14 03:34:52 +1000

    Trying new things
    https://jsfiddle.net/prisoner849/a2ogz9vx/

--- makePlanets.js
+++ makePlanets.js
@@ -1,41 +1,36 @@
-var points = [];
+var planets = [];
+var distances = [];
+var orbits = [];
+var light;
 
 function generatePlanets() {
-  var distances = [];
-  var planets = [];
-  var sun_point = new THREE.Object3D();
-  var earth_point = new THREE.Object3D();
+
+  orbits.push(Date.now()*0.0001);
+  orbits.push(Date.now()*0.0001);
 
   //Adding distances to array
   distances.push(100);
   distances.push(15);
   //Sun
   var sun_mesh = new Star(10, "rgb(255, 153, 51)");
-  var light = new THREE.PointLight(new THREE.Color(1, 1, 1), 2);
+  light = new THREE.PointLight(new THREE.Color("rgb(255, 153, 51)"), 2);
   sun_mesh.add(light)
-  scene.add(sun_mesh);
 
   //Create meshes
   var earth_mesh = new Planet(3, "rgb(66, 134, 244)");
   var moon_mesh = new Planet(1, "rgb(166, 166, 166)");
 
-  //Attach points to meshes
-  sun_mesh.add(sun_point);
-  earth_mesh.add(earth_point);
+  earth_mesh.position.set(Math.cos(orbits[0]) *  distances[0],
+                          0, Math.sin(orbits[0]) * distances[0]);
 
   //Add planets to array
+  planets.push(sun_mesh);
   planets.push(earth_mesh);
   planets.push(moon_mesh);
 
-  //Add points to array
-  points.push(sun_point);
-  points.push(earth_point);
-
   //Set planet positions
-  planets[0].position.set(distances[0], 0 , 0);
-  planets[1].position.set(distances[1], 0 , 0);
+  //planets[1].position.set(distances[0], 0 , 0);
+  planets[2].position.set(distances[1], 0 , 0);
 
-  //Adding orbits
-  points[0].add(earth_mesh);
-  points[1].add(moon_mesh);
+  scene.add(planets[0]);
 }
