c1de07b869b007e217e082be1c14c880026de195
CHOsen One <37858778+leoncho918@users.noreply.github.com>
2019-05-16 01:45:10 +1000

    Created functions for everything
    Created functions to make stars, planets, moons.
    Created function to dynamically generate gui settings for all stars, planets and moons

--- gui.js
+++ gui.js
@@ -11,19 +11,12 @@ function buildGui() {
       colour: star.material.color.getHex()
     }
 
-    var star_folder = gui.addFolder(star.userData.name);
+    var star_folder = stars_gui.addFolder(star.userData.name);
 
-    star_folder.addColor(star_properties, 'colour', 0, 0.1).onChange(function(val)
-    {
-      star.material.color.setHex(val);
-      star.material.emissive.setHex(val);
-      star.children[0].color.setHex(val);
-    });
+    addStarGui(star_properties, star_folder, 'colour', star, true, 0, 0.1);
+    addStarGui(star_properties, star_folder, 'size', star, false, 0.1, 10);
 
-    star_folder.add(star_properties, 'size', 0.1, 10).onChange(function(val)
-    {
-      star.scale.set(val, val, val);
-    });
+
   });
 
   var planets_gui = gui.addFolder("Planets");
@@ -36,6 +29,68 @@ function buildGui() {
       speed: planet.userData.speed
     }
 
-    var planet_folder = gui.addFolder(planet.userData.name);
-  })
+    var planet_folder = planets_gui.addFolder(planet.userData.name);
+
+    addItemGui(planet_properties, planet_folder, 'colour', planet, true, false, 0, 0.1);
+    addItemGui(planet_properties, planet_folder, 'size', planet, false, true, 0.1, 10);
+    addItemGui(planet_properties, planet_folder, 'orbit', planet, false, false, 0, 1000);
+    addItemGui(planet_properties, planet_folder, 'speed', planet, false, false, 0, 100);
+  });
+
+  var moons_gui = gui.addFolder("Moons");
+  moons.forEach(function(moon)
+  {
+    var moon_properties = {
+      size: moon.scale.x,
+      colour: moon.material.color.getHex(),
+      orbit: moon.userData.orbit,
+      speed: moon.userData.speed
+    }
+
+    var moon_folder = moons_gui.addFolder(moon.userData.name);
+
+    addItemGui(moon_properties, moon_folder, 'colour', moon, true, false, 0, 0.1);
+    addItemGui(moon_properties, moon_folder, 'size', moon, false, true, 0.1, 5);
+    addItemGui(moon_properties, moon_folder, 'orbit', moon, false, false, 0, 50);
+    addItemGui(moon_properties, moon_folder, 'speed', moon, false, false, 0, 100);
+  });
+}
+
+function addStarGui(properties, folder, name, value, isColour, min, max) {
+  if (isColour) {
+    folder.addColor(properties, name, min, max).onChange(function(val) {
+      value.material.color.setHex(val);
+      value.material.emissive.setHex(val);
+      value.children[0].color.setHex(val);
+    });
+  }
+  else {
+    folder.add(properties, name, min, max).onChange(function(val) {
+      value.scale.set(val, val, val);
+    });
+  }
+}
+
+function addItemGui(properties, folder, name, value, isColour, isSize, min, max) {
+  if (isColour || isSize) {
+    if (isColour) {
+      folder.addColor(properties, name, min, max).onChange(function(val) {
+        value.material.color.setHex(val);
+      });
+    }
+    if (isSize) {
+      folder.add(properties, name, min, max).onChange(function(val) {
+        value.scale.set(val, val, val);
+      });
+    }
+  }
+  else {
+    folder.add(properties, name, min, max).onChange(function(val)
+      {
+        if (name == 'orbit')
+          value.userData.orbit = val;
+        else
+          value.userData.speed = val;
+      });
+  }
 }
